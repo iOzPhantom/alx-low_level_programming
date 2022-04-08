69a7e95de8dddb5b4c9946ebb313cf741d795439
Ozemoya Iriogbe <iriogbe33@gmail.com>
2022-04-08 04:14:18 +0100

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int str_len(char *str);
-void *_calloc(unsigned int bytes, unsigned int size);
+int str_len(char *str);void *_calloc(unsigned int bytes, unsigned int size);
 void add_arrays(int *mul_result, int *sum_result, int len_r);
 int is_digit(char c);
 int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r);
