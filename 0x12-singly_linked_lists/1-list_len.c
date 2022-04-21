5ca53b44c4c7d8fec76f6f803f422397c1a71cd6
Ozemoya Iriogbe <iriogbe33@gmail.com>
2022-04-21 23:38:54 +0100

    1 list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,18 @@
 #include "lists.h"
+
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - prints number of nodes
+ * @h: first parameter
+ * Return: number of elements
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
-
-    nelem = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+unsigned int i = 0;
+while (h != NULL)
+{
+h = h->next;
+i++;
+}
+return (i);
 }
