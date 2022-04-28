13c844d48e90c4a6a2fd229488c407dab945403c
Ozemoya Iriogbe <iriogbe33@gmail.com>
2022-04-28 20:39:20 +0100

    0x14 project start

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,5 @@
-#ifndef _MAIN_
-#define _MAIN_
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
